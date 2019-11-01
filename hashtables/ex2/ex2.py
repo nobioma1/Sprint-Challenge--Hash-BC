52ad786d1231a79be63ab15ea7141f85aee877da
Noble Obioma <nobioma1@gmail.com>
2019-11-01 11:58:14 +0100

    Implement  reconstruct_trip

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,4 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (HashTable, hash_table_insert, hash_table_retrieve)
 
 
 class Ticket:
@@ -16,8 +11,23 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # for all the the ticket in the array of given tickets
+    for ticket in tickets:
+        # insert into hash tables
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    # get the first item for the route where key is NONE
+    first_ticket = hash_table_retrieve(hashtable, 'NONE')
+    # insert it into the first position on the route
+    route[0] = first_ticket
+
+    # for the rest of the space in the route
+    for i in range(1, len(route)):
+        # get the ticket before the next to be inserted from the route
+        preceding_ticket = route[i - 1]
+        # retrieve the value using the preceding ticket from the hastable
+        ticket = hash_table_retrieve(hashtable, preceding_ticket)
+        # insert it into postion in routes
+        route[i] = ticket
 
     return route
